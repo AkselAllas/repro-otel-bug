af2102cbb4aa9d90a07d0c824262bbc9a972554b
Aksel Allas <aksel.allas@pactum.com>
2023-01-23 11:51:48 +0200

    Working node SDK setup with awaiting differently

--- tracing.js
+++ tracing.js
@@ -13,75 +13,65 @@ const {
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { NodeSDK } = require('@opentelemetry/sdk-node')
 
+const asyncFun = async () => {
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG)
+  
+  const traceExporter = new OTLPTraceExporter()
+  
+  const sdk = new NodeSDK({
+    traceExporter,
+    spanProcessor: new SimpleSpanProcessor(traceExporter),
+  });
+  
+  await sdk
+    .start()
+  
+  const tracer = trace.getTracer('example-otlp-exporter-node');
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG)
-
-const traceExporter = new OTLPTraceExporter()
-
-const provider = new BasicTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]:
-      process.env.DD_SERVICE || 'default-node-service-name',
-  }),
-});
-
-provider.addSpanProcessor(new SimpleSpanProcessor(traceExporter));
-
-const sdk = new NodeSDK({
-  traceExporter,
-});
-
-sdk
-  .start()
-  .then(() => console.log('OTEL Starting...'))
-  .catch((error) => console.error(error))
-  .finally(() => console.info('OTEL start completed'))
-
-provider.register();
-
-const tracer = trace.getTracer('example-otlp-exporter-node');
-
-// Create a span. A span must be closed.
-const parentSpan = tracer.startSpan('main');
-for (let i = 0; i < 10; i += 1) {
-  doWork(parentSpan);
-}
-// Be sure to end the span.
-parentSpan.end();
-
-setTimeout(() => {
-  // flush and close the connection.
-  sdk
-  .shutdown()
-  .then(() => console.log('Tracing terminated'))
-  .catch((error) => console.error('Error terminating tracing', error))
-  .finally(() => console.log('Shutting down...'))
- }, 2000);
-
-function doWork(parent) {
-  // Start another span. In this example, the main method already started a
-  // span, so that'll be the parent span, and this will be a child span.
-  const ctx = trace.setSpan(context.active(), parent);
-  const span = tracer.startSpan('doWork', undefined, ctx);
-
-  // simulate some random work.
-  for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
-    // empty
+  function doWork(parent) {
+    // Start another span. In this example, the main method already started a
+    // span, so that'll be the parent span, and this will be a child span.
+    const ctx = trace.setSpan(context.active(), parent);
+    const span = tracer.startSpan('doWork', undefined, ctx);
+  
+    // simulate some random work.
+    for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {
+      // empty
+    }
+    // Set attributes to the span.
+    span.setAttribute('key', 'value');
+  
+    span.setAttribute('mapAndArrayValue', [
+      0, 1, 2.25, 'otel', {
+        foo: 'bar',
+        baz: 'json',
+        array: [1, 2, 'boom'],
+      },
+    ]);
+  
+    // Annotate our span to capture metadata about our operation
+    span.addEvent('invoking doWork');
+  
+    // end span
+    span.end();
   }
-  // Set attributes to the span.
-  span.setAttribute('key', 'value');
 
-  span.setAttribute('mapAndArrayValue', [
-    0, 1, 2.25, 'otel', {
-      foo: 'bar',
-      baz: 'json',
-      array: [1, 2, 'boom'],
-    },
-  ]);
-
-  // Annotate our span to capture metadata about our operation
-  span.addEvent('invoking doWork');
-
-  // end span
-  span.end();
+  
+  // Create a span. A span must be closed.
+  const parentSpan = tracer.startSpan('main');
+  for (let i = 0; i < 10; i += 1) {
+    doWork(parentSpan);
+  }
+  // Be sure to end the span.
+  parentSpan.end();
+  
+  setTimeout(() => {
+    // flush and close the connection.
+    sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.error('Error terminating tracing', error))
+    .finally(() => console.log('Shutting down...'))
+   }, 2000);
 }
+asyncFun()
